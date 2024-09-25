0c378e6914896a9bf053b99a8a36b1763a1cb9ab
rodionmaulenov <natalia.maulenova@gmail.com>
2024-09-25 14:13:03 +0300

    new commit

--- schedule/templatetags/breadcrumb_tags.py
+++ schedule/templatetags/breadcrumb_tags.py
@@ -3,10 +3,21 @@ from django import template
 register = template.Library()
 
 
-@register.filter
-def split_path(value):
-    """
-    Splits the request path and returns a list of segments.
-    Example: '/admin/schedule/moldova/' -> ['admin', 'schedule', 'moldova']
-    """
-    return value.strip('/').split('/')
+@register.simple_tag
+def generate_breadcrumbs(request):
+    breadcrumb_list = [{'name': 'Home', 'url': '/admin'}]
+
+    path = request.path.strip('/').split('/')
+
+    accumulated_path = '/admin'
+    for segment in path[1:]:
+        if segment.isdigit():
+            continue
+
+        accumulated_path += f'/{segment}'
+        breadcrumb_list.append({
+            'name': segment.capitalize(),
+            'url': accumulated_path
+        })
+
+    return breadcrumb_list
