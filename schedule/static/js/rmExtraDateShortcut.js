c18c0e01b81a7702575007a05c15f821e66ea021
rodionmaulenov <natalia.maulenova@gmail.com>
2024-10-15 11:37:12 +0300

    change get_queryset

--- schedule/static/js/rmExtraDateShortcut.js
+++ schedule/static/js/rmExtraDateShortcut.js
@@ -1,18 +1,42 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const observer = new MutationObserver(function (mutationsList) {
+//         for (const mutation of mutationsList) {
+//             if (mutation.type === 'childList') {
+//                 cleanupRedundantLinkAndWarnings()
+//                 addListenersForClick()
+//             }
+//         }
+//     });
+//
+//     const targetNode = document.querySelector('#result_list')
+//     if (targetNode) {
+//         observer.observe(targetNode, {childList: true, subtree: true}) // Observe the entire subtree
+//     }
+// });
 document.addEventListener("DOMContentLoaded", function () {
+    let scheduled = false;
+
     const observer = new MutationObserver(function (mutationsList) {
-        for (const mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                cleanupRedundantLinkAndWarnings()
-                addListenersForClick()
-            }
+        if (!scheduled) {
+            scheduled = true;
+            setTimeout(() => {
+                for (const mutation of mutationsList) {
+                    if (mutation.type === 'childList') {
+                        cleanupRedundantLinkAndWarnings();
+                        addListenersForClick();
+                    }
+                }
+                scheduled = false;
+            }, 30); // Slight delay to batch DOM updates
         }
     });
 
-    const targetNode = document.querySelector('#result_list')
+    const targetNode = document.querySelector('#result_list');
     if (targetNode) {
-        observer.observe(targetNode, {childList: true, subtree: true}) // Observe the entire subtree
+        observer.observe(targetNode, { childList: true, subtree: true }); // Observe the entire subtree
     }
 });
+
 
 
 function cleanupRedundantLinkAndWarnings() {
